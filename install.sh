dc28ee454355e3e7f612fed286df6a48d30b7844
Scott Lee <soulindarkness@gmail.com>
2021-02-16 00:01:18 +0900

    Add home branch

--- install.sh
+++ install.sh
@@ -8,14 +8,15 @@ fi
 echo "심볼릭 릭크 생성..."
 ln -sfv ~/.vimconfig/vimrc ~/.vimrc
 
-echo "vundle 다운로드중..."
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "plug 다운로드중..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim/colors
 cp ./colors/CodeSchool3.vim ~/.config/nvim/colors/
 
 echo "vundle 설치중..."
-vim -c :BundleInstall -c :qa
+vim -c :PlugInstall -c :qa
 
 echo "rtags 설치중..."
 mkdir ~/.rdm
